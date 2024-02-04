e8a19cdb08209295e5050a8c88f525d87a9ac518
Jash-Vora <133149276+Jash-Vora@users.noreply.github.com>
2024-02-04 09:58:10 +0530

    Add files via upload

---
+++ React/src/App.tsx
@@ -0,0 +1,10 @@
+
+
+
+function App(){
+  let h1 = document.createElement("h1");
+  h1.innerHTML = "Hello World";
+  return <>{h1}</>
+}
+
+export default App;
---
+++ React/src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+let h1 = document.createElement("h1");
+h1.innerHTML = "Hello World";
+document.body.appendChild(h1);
